dec8b4644128ebe4191509daae7948dc371ab8ac
JustinLung <justinlung77@gmail.com>
2022-09-30 22:50:22 +0200

    WIP: header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,38 @@
+<header class="header">
+	<a href="/" class="header__link">
+		<img
+			src="/assets/images/logo-ilojo-bar.webp"
+			alt="Logo Ilojo Bar"
+			class="header__link__image"
+		/>
+	</a>
+	<p class="header__text">Discover Untold Stories</p>
+</header>
+
+<style>
+	.header {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		padding: 2rem 5rem;
+	}
+
+	.header__link__image {
+		width: 10rem;
+	}
+
+	@media (max-width: 50rem) {
+		.header {
+			padding: 2rem 1rem;
+		}
+
+		.header__link__image {
+			width: 8rem;
+		}
+
+		.header__text {
+			display: none;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
-    import '../styles/style.css'
-    import '../styles/theme.css'
+	import '../styles/style.css';
+	import '../styles/theme.css';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
+<Header />
 <main>
 	<slot />
 </main>
+
+<style>
+	main {
+		padding: 2rem 5rem;
+	}
+
+	@media (max-width: 50rem) {
+		main {
+			padding: 2rem 1rem;
+		}
+	}
+</style>
