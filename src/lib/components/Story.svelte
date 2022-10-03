cf6e67b1d8074b9bc64e69c9f02e349e0337fbc7
JustinLung <justinlung77@gmail.com>
2022-10-03 13:34:19 +0200

    WIP story component

---
+++ src/lib/components/Story.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	export let storyTitle: string = 'Story Image';
+	export let storyImage: string;
+</script>
+
+<a href="/">
+	<figure class="frame">
+		<img src={storyImage} alt={storyTitle} class="frame__image" />
+	</figure>
+</a>
+
+<style>
+	a {
+		position: relative;
+		text-decoration: none;
+		width: 100%;
+	}
+
+	.frame {
+		background-image: url('/assets/images/image-frame.png');
+		background-size: contain;
+		background-repeat: no-repeat;
+		background-position: center;
+		margin: 0;
+		aspect-ratio: 127/155;
+		position: relative;
+		overflow: hidden;
+	}
+
+	.frame__image {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: 95%;
+		height: 95%;
+		object-fit: cover;
+		aspect-ratio: 127/155;
+		z-index: -1;
+		position: relative;
+	}
+
+	@media (max-width: 40rem) {
+		.frame {
+			margin: 0 auto;
+		}
+	}
+</style>
