d8f86dfa2a01b18b5f878635586fecaed1095418
JustinLung <justinlung77@gmail.com>
2022-10-09 14:28:14 +0200

    added animations to story page

--- src/routes/stories/[id]/+page.svelte
+++ src/routes/stories/[id]/+page.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
+	import gsap from 'gsap';
+	import { onMount } from 'svelte';
 	import Button from '$lib/components/Button.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 	const { story } = data.data;
+
+	onMount(() => {
+		gsap.fromTo(
+			'.story__title',
+			{ opacity: 0, y: -50 },
+			{ opacity: 1, y: 0, duration: 1.3, ease: 'bounce.out' }
+		);
+		gsap.to('.story__author', {
+			duration: 1,
+			opacity: 1,
+			stagger: 0.3,
+			delay: 0.5
+		});
+		gsap.to('.story__content', {
+			duration: 1,
+			opacity: 1,
+			stagger: 0.3,
+			delay: 0.8
+		});
+	});
 </script>
 
 <svelte:head>
@@ -29,12 +51,14 @@
 	}
 	.story__title {
 		font-size: 2.5rem;
+		opacity: 0;
 	}
 
 	.story__author {
 		font-size: 1.5rem;
 		font-weight: bold;
 		padding: 1rem 0;
+		opacity: 0;
 	}
 
 	.story__content {
@@ -44,6 +68,7 @@
 		justify-content: center;
 		align-items: center;
 		gap: 1rem;
+		opacity: 0;
 	}
 
 	.story__buttons {
