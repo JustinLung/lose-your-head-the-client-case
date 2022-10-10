ce01bf1d40614457211aac692a08c501d956a571
JustinLung <justinlung77@gmail.com>
2022-10-10 10:48:03 +0200

    removed button component and used a tag

--- src/routes/stories/[id]/+page.svelte
+++ src/routes/stories/[id]/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import gsap from 'gsap';
 	import { onMount } from 'svelte';
-	import Button from '$lib/components/Button.svelte';
 	import type { PageData } from './$types';
 	import { storyId } from '$lib/stores/store';
 	import { page } from '$app/stores';
-	import { goto } from '$app/navigation';
 
 	export let data: PageData;
 	const { story } = data.data;
@@ -20,15 +18,6 @@
 	}
 
 	const [next, previous] = getPreviousAndNext(currentId);
-
-	function navigateToStory(index: number) {
-		const id = $storyId[index];
-		const url = `/stories/${id}`;
-		goto(url, { noscroll: false });
-		setTimeout(() => {
-			window.location.reload();
-		}, 300);
-	}
 
 	onMount(() => {
 		gsap.fromTo(
@@ -61,9 +50,11 @@
 	<div class="story__content">
 		{@html story.content.html}
 	</div>
-	<div class="story__buttons">
-		<Button on:click={() => navigateToStory(previous)}>Previous Story</Button>
-		<Button on:click={() => navigateToStory(next)}>Next Story</Button>
+	<div class="story__links">
+		<a data-sveltekit-reload href={`/stories/${$storyId[previous]}`} class="story__links--link"
+			>Previous</a
+		>
+		<a data-sveltekit-reload href={`/stories/${$storyId[next]}`} class="story__links--link">Next</a>
 	</div>
 </section>
 
@@ -94,12 +85,29 @@
 		opacity: 0;
 	}
 
-	.story__buttons {
+	.story__links {
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
 		align-items: center;
 		margin: 1.5rem 0 0;
+	}
+
+	.story__links--link {
+		position: relative;
+		border-radius: 0.7rem;
+		padding: 1rem 2rem;
+		filter: contrast(110%) brightness(100%);
+		background: linear-gradient(40deg, rgba(195, 165, 128, 1), rgba(0, 0, 0, 0)),
+			url("data:image/svg+xml,%3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
+		color: var(--color-white);
+		box-shadow: var(--box-shadow);
+		text-decoration: none;
+		transition: 0.3s ease-in-out;
+	}
+
+	.story__links--link:hover {
+		transform: scale(1.2);
 	}
 
 	:global(img) {
