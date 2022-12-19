88bbafa33c3d560b6591b1a724935b081c6889b3
JustinLung <justinlung77@gmail.com>
2022-12-19 17:19:23 +0100

    footer fixed

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,15 +1,103 @@
-<footer class="footer">&#169; {new Date().getFullYear()} Legacy &#183; All Rights Reserved</footer>
+<script lang="ts">
+	export let companyName: string;
+</script>
+
+<footer>
+	<section class="footer-copyright">
+		<a href="/">
+			<img
+				src="/assets/images/logo-ilojo-bar.webp"
+				alt="Logo Ilojo Bar"
+				width="200"
+				height="auto"
+				loading="lazy"
+			/>
+		</a>
+		<p>&#169; {new Date().getFullYear()} {companyName}</p>
+	</section>
+
+	<section class="footer-links">
+		<h2>Stories</h2>
+		<slot />
+	</section>
+
+	<section class="footer-links-pages">
+		<h2 class="footer-links__header">Menu</h2>
+		<a href="/" class="footer-link">Home</a>
+		<a href="/" class="footer-link">Discover Stories</a>
+	</section>
+</footer>
 
 <style>
-	.footer {
-		max-width: 80rem;
-		margin: 2rem auto 2rem;
+	footer {
+		padding: 3em;
+		color: var(--color-white);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		text-align: center;
+		gap: 0;
+		background-color: var(--color-black);
 	}
 
-	@media (max-width: 80rem) {
-		.footer {
-			text-align: center;
-			padding-bottom: 1.5rem;
+	a {
+		text-decoration: none;
+		color: var(--color-white);
+		font-size: 1.2rem;
+		line-height: 1.5;
+		text-decoration: none;
+		width: fit-content;
+	}
+
+	.footer-link::after {
+		content: '';
+		display: block;
+		width: 100%;
+		transform: scaleX(0);
+		height: 3px;
+		left: 0;
+		background-color: var(--color-white);
+		transform-origin: bottom left;
+		transition: transform 0.5s;
+	}
+
+	.footer-link:hover::after {
+		transform: scaleX(1);
+	}
+
+	.footer-copyright {
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5em;
+	}
+
+	.footer-links {
+		display: none;
+	}
+
+	.footer-links__header {
+		margin-bottom: 0.25em;
+	}
+
+	@media (min-width: 70em) {
+		footer {
+			text-align: left;
+			display: flex;
+			justify-content: center;
+			flex-direction: row;
+			gap: 2em;
+		}
+
+		.footer-links-pages,
+		.footer-links {
+			display: flex;
+			flex-direction: column;
+			align-items: baseline;
+		}
+
+		.footer-links {
+			gap: .5rem;
 		}
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import '../styles/style.css';
 	import Header from '$lib/components/Header.svelte';
-	import BigFooter from 'ilojo-bar-components/components/BigFooter.svelte';
-	// import Footer from '$lib/components/Footer.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 	import BackToTop from '$lib/components/BackToTop.svelte';
 	import type { PageData } from './$types';
 
@@ -19,7 +18,12 @@
 	<slot />
 </main>
 <BackToTop />
-<BigFooter companyName="Legacy"/>
+
+<Footer companyName="Legacy">
+	{#each data.stories as story}
+		<a data-sveltekit-reload href="/stories/{story.id}" class="footer-link">{story.title}</a>
+	{/each}
+</Footer>
 
 <style>
 	main {
@@ -39,6 +43,31 @@
 		color: var(--color-light-red);
 	}
 
+	.footer-link {
+		text-decoration: none;
+		color: var(--color-white);
+		font-size: 1.2rem;
+		line-height: 1.5;
+		text-decoration: none;
+		width: fit-content;
+		max-width: 19rem;
+	}
+
+	.footer-link::after {
+		content: '';
+		display: block;
+		width: 100%;
+		transform: scaleX(0);
+		height: 3px;
+		left: 0;
+		background-color: var(--color-white);
+		transform-origin: bottom left;
+		transition: transform 0.5s;
+	}
+
+	.footer-link:hover::after {
+		transform: scaleX(1);
+	}
 	@media (max-width: 80rem) {
 		main {
 			padding: 2rem 1rem;
