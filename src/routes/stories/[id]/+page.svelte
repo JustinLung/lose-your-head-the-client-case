abdb47784e3202cb96fe018959506e5926ca6a83
DphnZwp <zwuupdaphne@gmail.com>
2022-12-22 13:16:40 +0100

    css variable for link background

--- src/routes/stories/[id]/+page.svelte
+++ src/routes/stories/[id]/+page.svelte
@@ -213,8 +213,7 @@
 		border-radius: 0.7rem;
 		padding: 1rem 2rem;
 		filter: contrast(110%) brightness(100%);
-		background: linear-gradient(40deg, #b38c5d, #00000000),
-			url("data:image/svg+xml,%3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
+		background: var(--link-background);
 		color: var(--color-white);
 		box-shadow: var(--box-shadow);
 		text-decoration: none;
